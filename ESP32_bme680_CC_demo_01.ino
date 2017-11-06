9003fde12a5c45134b441ffe1cdf872c137b6179
G6EJD <d.l.bird@sky.com>
2017-11-06 09:55:38 +0000

    Update ESP32_bme680_CC_demo_01.ino
    Updated copyright statement

--- ESP32_bme680_CC_demo_01.ino
+++ ESP32_bme680_CC_demo_01.ino
@@ -84,14 +84,15 @@ void loop() {
   Serial.println("R\n");
   
 // The MIT License (MIT) Copyright (c) 2017 by David Bird.
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, but
-// not to use it commercially for profit making or to sub-license and/or to sell copies of the Software or to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions: 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, but not to use it commercially for profit making or to sub-license and/or to sell copies of the Software or to
+// permit persons to whom the Software is furnished to do so, subject to the following conditions: 
 //   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//   LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // See more at http://dsbird.org.uk
   
   //Calculate humidity contribution to IAQ index
