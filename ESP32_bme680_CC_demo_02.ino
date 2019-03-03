b263e591c3c29342989bf240bd0599d2087252bc
G6EJD <d.l.bird@sky.com>
2019-03-03 15:02:28 +0000

    Update ESP32_bme680_CC_demo_02.ino
    Correct gas average value

--- ESP32_bme680_CC_demo_02.ino
+++ ESP32_bme680_CC_demo_02.ino
@@ -135,7 +135,7 @@ void GetGasReference(){
   // Now run the sensor for a burn-in period, then use combination of relative humidity and gas resistance to estimate indoor air quality as a percentage.
   Serial.println("Getting a new gas reference value");
   int readings = 10;
-  for (int i = 0; i <= readings; i++){ // read gas for 10 x 0.150mS = 1.5secs
+  for (int i = 1; i <= readings; i++){ // read gas for 10 x 0.150mS = 1.5secs
     gas_reference += bme.readGas();
   }
   gas_reference = gas_reference / readings;
